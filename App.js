2a53acf80e19d8aef8028bf6d557d704544cfb21
aluno <aluno@udicentro.local>
2026-08-06 16:47:09 -0300

    resolvendo erros

--- App.js
+++ App.js
@@ -1,37 +1,8 @@
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text } from 'react-native';
 
-function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home screen</Text>
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  );
-}
-
-function Profile() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (<View><Text>IFTM</Text></View>
   );
 }
 export default App;
